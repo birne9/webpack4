52993405c4b3dd68aabb00b9f609d5886c51c7ee
birne9 <17317296268@163.com>
2025-04-17 14:05:18 +0800

    feat:add css prefix

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const resolve = (dir) => path.join(__dirname, "../", dir);
 module.exports = {
@@ -15,17 +15,18 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
-  plugins:[
+  plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html', // 指定 HTML 模板文件
-      filename: 'index.html',       // 生成的 HTML 文件名
-      inject: 'body',              // 将 JS 注入到 body 底部
-      minify: {                    // 压缩 HTML（可选）
+      template: "./src/index.html", // 指定 HTML 模板文件
+      filename: "index.html", // 生成的 HTML 文件名
+      inject: "body", // 将 JS 注入到 body 底部
+      minify: {
+        // 压缩 HTML（可选）
         collapseWhitespace: true,
         removeComments: true,
       },
